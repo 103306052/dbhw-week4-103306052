82ff5981aeff331396d09c942d7051bc73d8c75d
103306052 <103306052@nccu.edu.tw>
2016-04-03 21:12:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=f4a75ba9-7721-4363-884d-c3820b0b917c";
-var API_PATH = "http://data.kaohsiung.gov.tw/Opendata/DownLoad.aspx?Type=2&CaseNo1=AV&CaseNo2=2&FileType=1&Lang=C&FolderType=";
+var API_PATH = "http://opendata.khcc.gov.tw/public/OD_art_program.ashx?SDate=2016/03/29&EDate=2016/09/29";
 function getData(cb) {
   $.ajax({
     url : API_PATH,
